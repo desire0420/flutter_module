1f76bceddb1a18963caa0d6da05cf23d79fd3f4f
王伟 <wei.wang23@zhenai.com>
2019-07-08 11:29:29 +0800

    network

--- flutter_module/lib/demo/demo_http/net/net_mode_entity.dart
+++ flutter_module/lib/demo/demo_http/net/net_mode_entity.dart
@@ -1,10 +1,9 @@
 class NetModeEntity {
   List<NetModeResult> result;
-  int code;
 
-  String message;
-
-  NetModeEntity({this.result, this.code, this.message});
+  NetModeEntity({
+    this.result,
+  });
 
   NetModeEntity.fromJson(Map<String, dynamic> json) {
     if (json['result'] != null) {
@@ -13,18 +12,6 @@ class NetModeEntity {
         result.add(new NetModeResult.fromJson(v));
       });
     }
-    code = json['code'];
-    message = json['message'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.result != null) {
-      data['result'] = this.result.map((v) => v.toJson()).toList();
-    }
-    data['code'] = this.code;
-    data['message'] = this.message;
-    return data;
   }
 }
 
