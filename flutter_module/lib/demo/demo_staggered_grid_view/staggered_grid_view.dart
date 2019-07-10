2d6c679c95efa5b953314f7143d65d0e5f736d14
王伟 <wei.wang23@zhenai.com>
2019-07-10 14:49:00 +0800

    x

--- flutter_module/lib/demo/demo_staggered_grid_view/staggered_grid_view.dart
+++ flutter_module/lib/demo/demo_staggered_grid_view/staggered_grid_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_module/common/app_bar_title.dart';
 
 
 class StaggeredGridViewDemo extends StatefulWidget{
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
