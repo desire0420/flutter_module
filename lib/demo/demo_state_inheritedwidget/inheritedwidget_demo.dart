084da65536e6096ba426e3fdb6065cd3599dd1da
王伟 <wei.wang23@zhenai.com>
2019-08-09 13:50:16 +0800

    組件通讯

--- lib/demo/demo_state_inheritedwidget/inheritedwidget_demo.dart
+++ lib/demo/demo_state_inheritedwidget/inheritedwidget_demo.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_module/common/ui_help.dart';
 
+import 'count_mode.dart';
+
 class InheritedWidgetDemo extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -10,7 +12,7 @@ class InheritedWidgetDemo extends StatefulWidget {
 }
 
 class InheritedWidgetDemoState extends State<InheritedWidgetDemo> {
-  int _count = 0;
+  int _count = 1;
 
   void _incerseCount() {
     print('--------------------');
@@ -22,28 +24,36 @@ class InheritedWidgetDemoState extends State<InheritedWidgetDemo> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('InheritedWidget'),
+    return CounterProvider(
+      count: _count,
+      increaseCount: _incerseCount,
+      child: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('InheritedWidget'),
+        ),
+//        body: Counter(
+//          count: _count,
+//          increaseCount: _incerseCount,
+//        ),
+        body: Counter(),
+        floatingActionButton: FloatingActionButton(onPressed: _incerseCount),
       ),
-      body: Counter(
-        count: _count,
-        increaseCount: _incerseCount,
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: _incerseCount),
     );
   }
 }
 
 class Counter extends StatelessWidget {
-  final int count;
-  final VoidCallback increaseCount;
-
-   //父布局传递increaseCount回调给子目录
-  const Counter({Key key, this.count, this.increaseCount}) : super(key: key);
+//  final int count;
+//  final VoidCallback increaseCount;
+//
+//  //父布局传递increaseCount回调给子目录
+//  const Counter({Key key, this.count, this.increaseCount}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final int count = CounterProvider.of(context).count;
+    final VoidCallback increaseCount =
+        CounterProvider.of(context).increaseCount;
     return Container(
       child: Column(
         children: <Widget>[
